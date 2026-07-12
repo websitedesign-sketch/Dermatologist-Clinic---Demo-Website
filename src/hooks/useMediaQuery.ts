e40d901840fefb312a82c0ac1b33a00eb8604616
websitedesign-sketch <abhixyz20000@gmail.com>
2026-07-12 15:20:36 +0530

    feat: create useMediaQuery hook

---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+
+export default function useMediaQuery(query: string): boolean {
+  const [matches, setMatches] = useState<boolean>(false)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(query)
+    setMatches(mediaQuery.matches)
+
+    const handler = (e: MediaQueryListEvent) => setMatches(e.matches)
+    mediaQuery.addEventListener('change', handler)
+
+    return () => mediaQuery.removeEventListener('change', handler)
+  }, [query])
+
+  return matches
+}
